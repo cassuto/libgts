d8feca1b74b2252e43864cd9bdf408d1da579116
cassuto <diyer175@hotmail.com>
2020-05-13 23:38:25 +0800

    Updating classify algorithm.

--- Classifier/ChordClassifier.cc
+++ Classifier/ChordClassifier.cc
@@ -1,38 +1,42 @@
+#include <cmath>
 #include <cstring>
 #include <cassert>
+#include <algorithm>
 #include "ChordLibrary.h"
 #include "ChordClassifier.h"
 
 ChordClassifier::ChordClassifier()
+	: m_biasBeta(1.06) // P87 Section 2.4
 {
-	makeBitmask();
+	m_chordCount = ChordLibrary::chordEntryCount * 12;
+	assert(m_chordCount <= MAX_N_CHORDS);
+
+	makeBitmaskMatrix();
 }
 
-void ChordClassifier::makeBitmask()
+void ChordClassifier::makeBitmaskMatrix()
 {
-	const double v1 = 1, v2 = 1, v3 = 1;
-
 	std::memset(m_bitmask, sizeof m_bitmask, 0);
 
 	int p = 0;
-	for (int n = 0; n < ChordLibrary::chordsCount; ++n) {
-		const ChordLibrary::ChordBase *chd = ChordLibrary::chords[n];
+	for (int n = 0; n < ChordLibrary::chordEntryCount; n++) {
+		const ChordLibrary::ChordBase *chd = ChordLibrary::chordEntry[n];
 		for (int i = 0; i < 12; i++) {
 			switch (chd->type()) {
 			case 3: {
 				const ChordLibrary::Chord3 *chd3 = static_cast<const ChordLibrary::Chord3 *>(chd);
-				m_bitmask[p][(i + chd3->root) % 12] = v1;
-				m_bitmask[p][(i + chd3->third) % 12] = v2;
-				m_bitmask[p][(i + chd3->fifth) % 12] = v3;
+				m_bitmask[p][(i + chd3->root) % 12] = 1;
+				m_bitmask[p][(i + chd3->third) % 12] = 1;
+				m_bitmask[p][(i + chd3->fifth) % 12] = 1;
 				p++;
 				break;
 			}
 			case 7: {
 				const ChordLibrary::Chord7 *chd7 = static_cast<const ChordLibrary::Chord7 *>(chd);
-				m_bitmask[p][(i + chd7->root) % 12] = v1;
-				m_bitmask[p][(i + chd7->third) % 12] = v2;
-				m_bitmask[p][(i + chd7->fifth) % 12] = v3;
-				m_bitmask[p][(i + chd7->seventh) % 12] = v3;
+				m_bitmask[p][(i + chd7->root) % 12] = 1;
+				m_bitmask[p][(i + chd7->third) % 12] = 1;
+				m_bitmask[p][(i + chd7->fifth) % 12] = 1;
+				m_bitmask[p][(i + chd7->seventh) % 12] = 1;
 				p++;
 				break;
 			}
@@ -41,4 +45,56 @@ void ChordClassifier::makeBitmask()
 			}
 		}
 	}
+
+	assert(p == m_chordCount);
+}
+
+double ChordClassifier::weightChord(const std::vector<double> &chroma, double bitmask[], double bias, double N)
+{
+	double sum = 0;
+	for (int i = 0; i < 12; i++) {
+		sum = sum + ((1 - bitmask[i]) * (chroma[i]*chroma[i]));
+	}
+	return std::sqrt (sum) / ((12 - N) * bias); // Formula (3)
+}
+
+void ChordClassifier::classify(std::vector<double> chromaVector)
+{
+	// remove some of the 5th note energy from chroma vector
+	for (int i = 0; i < 12; i++) {
+		int fifth = (i+7) % 12;
+		chromaVector[fifth] -= 0.1 * chromaVector[i];
+		chromaVector[fifth] = std::max(0.0, chromaVector[fifth]);
+	}
+
+	// compute weights of each chord
+	for (int p = 0; p < m_chordCount; ++p) {
+		const ChordLibrary::ChordBase *chd = ChordLibrary::chordEntry[p];
+		double bias = chd->ghostNoteFixup ? m_biasBeta : 1.0;
+		switch (chd->type()) {
+		case 3:
+			m_weight[p] = weightChord(chromaVector, m_bitmask[p], bias, 3);
+			break;
+		case 7:
+			m_weight[p] = weightChord(chromaVector, m_bitmask[p], bias, 4);
+			break;
+		default:
+			assert(0);
+		}
+	}
+
+	// select a chord that minimized residual energy
+	int ans = 0;
+	double minWeight = 1e308;
+	for(int i = 0;i < m_chordCount; ++i) {
+		if (m_weight[i] < minWeight) {
+			minWeight = m_weight[i];
+			ans = i;
+		}
+	}
+
+	// read out chord info
+	m_root = ans % 12;
+	m_quality = ChordLibrary::chordEntry[ans]->quality;
+	m_intervals = ChordLibrary::chordEntry[ans]->intervals;
 }
--- Classifier/ChordClassifier.h
+++ Classifier/ChordClassifier.h
@@ -1,16 +1,30 @@
 #ifndef CHORDCLASSIFIER_H_
 #define CHORDCLASSIFIER_H_
 
+#include <vector>
+#include "ChordLibrary.h"
+
+#define MAX_N_CHORDS 256
+
 class ChordClassifier
 {
 public:
 	ChordClassifier();
 
+	void classify(std::vector<double> chromaVector);
+
 private:
-	void makeBitmask();
+	void makeBitmaskMatrix();
+	double weightChord(const std::vector<double> &chroma,  double bitmask[], double biasToUse, double N);
 
 protected:
-	double m_bitmask[108][12];
+	int m_chordCount;
+	double m_biasBeta;
+	double m_bitmask[MAX_N_CHORDS][12];
+	double m_weight[MAX_N_CHORDS];
+	int m_root;
+	int m_quality;
+	int m_intervals;
 };
 
 #endif // CHORDCLASSIFIER_H_
